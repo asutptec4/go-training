7aae0e3bad9b7d4c25b2c3d84de7190a0f47fe22
Alexander <asutp_tec4@tut.by>
2019-12-13 10:55:06 +0300

    add MaxRot

--- codewars/tasks/task2.go
+++ codewars/tasks/task2.go
@@ -1,6 +1,8 @@
 package tasks
 
 import (
+	"math"
+	"sort"
 	"strings"
 )
 
@@ -15,5 +17,34 @@ func (s MyString) IsUpperCase() bool {
 }
 
 func MaxRot(n int64) int64 {
-	return 0
+	var combs []int64
+	digits := convertToDigits(n)
+	for i := 0; len(digits)-i > 0; i++ {
+		combs = append(combs, convertFromDigits(digits))
+		rotPart := digits[:len(digits)-i]
+		last := rotPart[len(rotPart)-1]
+		for j := len(rotPart) - 1; j > 0; j-- {
+			rotPart[j] = rotPart[j-1]
+		}
+		rotPart[0] = last
+	}
+	sort.Slice(combs, func(i, j int) bool { return combs[i] > combs[j] })
+	return combs[0]
+}
+
+func convertToDigits(n int64) []int64 {
+	var r []int64
+	for n > 0 {
+		r = append(r, n%10)
+		n = n / 10
+	}
+	return r
+}
+
+func convertFromDigits(a []int64) int64 {
+	r := int64(0)
+	for i, v := range a {
+		r += v * int64(math.Pow10(i))
+	}
+	return r
 }
